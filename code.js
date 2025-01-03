fa26e741fd11b48e244ee05efe1363a608395077
Mwaura <101228726+Mwa-ura@users.noreply.github.com>
2025-01-03 15:34:10 +0300

    refactor makeGrid function.

--- code.js
+++ code.js
@@ -1,27 +1,24 @@
 // Etch-a-Sketch project
-const makeGrid = (columns) => {
-    let container = document.querySelector('#container');
-    for (let i = 0; i < (columns); i++) {
-        let column = document.createElement('div');
-        column.textContent = `${i +1}`;
-        column.style.border = 'black solid 1px';
-        column.setAttribute('class', 'gridClass');
-        column.onmouseover = hover;
-        container.appendChild(column);
-    }
+let containerGrid = document.querySelector('.container');
+let gridSize = 5;
+function makeGrid() {
+    for (let j = 0; j < gridSize; j++) {
+        const grid = document.createElement('div');
+        grid.classList.add('grid');
+        for (let i = 0; i < gridSize; i++) {
+            const squareSize = 600 / gridSize;
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.style.width = `${squareSize}px`;
+            square.style.height = `${squareSize}px`;
+            square.addEventListener('mouseover', function() {
+                square.style.backgroundColor = 'red';
+            });
+            grid.appendChild(square);
+        }
+        containerGrid.appendChild(grid);
+    }  
 }
-const hover = (ev) => {
-    ev.target.style.backgroundColor = 'red';
-}
-const askUser = () => {
-    return Number(prompt('Enter number of Grids.', 16))
-}
-window.onload = () => {
-    let button = document.querySelector('#userButton');
-    let container = document.querySelector('#container');
-    makeGrid(16);
-    button.addEventListener('click', () => {
-        makeGrid(askUser());
-    });
-    //askUser();
-}
+makeGrid();
+
+
