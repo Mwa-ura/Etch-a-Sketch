b906d129a7ac7e49dd8a555dc5723e8c61f838f3
Mwaura <101228726+Mwa-ura@users.noreply.github.com>
2025-01-30 21:17:24 +0300

    Update grid size input functionality and enhance button styling

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 // Etch-a-Sketch project
 let containerGrid = document.querySelector('.container');
-let gridSize = 5;
+let gridSize = 64;
 function makeGrid() {
     for (let j = 0; j < gridSize; j++) {
         const grid = document.createElement('div');
@@ -19,6 +19,17 @@ function makeGrid() {
         containerGrid.appendChild(grid);
     }  
 }
+function userInput() {
+    let input = prompt('Enter a number between 1 and 100');
+    if (input > 100 || input < 1) {
+        alert('Please enter a number between 1 and 100');
+        return;
+    }
+    gridSize = input;
+    containerGrid.innerHTML = '';
+    makeGrid();
+}
 makeGrid();
-
+const button = document.querySelector('#enterBtn');
+button.onclick = userInput;
 
